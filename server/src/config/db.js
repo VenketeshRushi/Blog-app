3c63956b7d22b510973d9920485267a01e7db058
Raman <rushivenketesh@gmail.com>
2022-12-20 20:29:40 +0530

    done project

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 const dotenv = require('dotenv')
 dotenv.config()
 const connect = () => {
-  return mongoose.connect(process.env.MOGODBCONNECTION);
+  return mongoose.connect("mongodb+srv://raman:raman@cluster0.fm7rpoi.mongodb.net/ecom",{ useNewUrlParser: true });
 };
 module.exports = connect;
